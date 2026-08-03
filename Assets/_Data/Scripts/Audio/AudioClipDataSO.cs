3c2ed9d3f6ac0f16f0ad72bc45e944175a5dee78
Hồ Thành Đạt <benjaminhojr@gmail.com>
2026-08-03 21:34:35 +0700

    ChooseMapPanel

--- Assets/_Data/Scripts/Audio/AudioClipDataSO.cs
+++ Assets/_Data/Scripts/Audio/AudioClipDataSO.cs
@@ -5,7 +5,13 @@ public class AudioClipDataSO : ScriptableObject
 {
     [SerializeField] private AudioClipName clipName = AudioClipName.None;
     [SerializeField] private AudioClip clip;
+    [SerializeField] private AudioChannel channel = AudioChannel.Sfx;
+    [SerializeField, Range(0f, 1f)] private float volume = 1f;
+    [SerializeField] private bool loop;
 
     public AudioClipName ClipName => clipName;
     public AudioClip Clip => clip;
+    public AudioChannel Channel => channel;
+    public float Volume => volume;
+    public bool Loop => loop;
 }
